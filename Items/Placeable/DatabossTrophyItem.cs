cda2583e94ab72e9f90fb433d0e37027b0aa3f97
MaybeDatapod <30482688+MaybeDatapod@users.noreply.github.com>
2019-05-30 23:00:38 -0500

    Add files via upload

---
+++ Items/Placeable/DatabossTrophyItem.cs
@@ -0,0 +1,28 @@
+using Terraria.ModLoader;
+
+namespace DataMod.Items.Placeable
+{
+	public class DatabossTrophyItem : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Databoss Trophy");
+			Tooltip.SetDefault("You had the right stuff");
+		}
+		public override void SetDefaults()
+		{
+			item.width = 30;
+			item.height = 30;
+			item.maxStack = 99;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;			
+			item.rare = 1;
+			item.createTile = mod.TileType("DatabossTrophy");
+			item.placeStyle = 0;
+		}
+	}
+}
