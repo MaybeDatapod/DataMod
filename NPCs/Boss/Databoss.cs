6aaa7056bec602b62d9a4478a85482b84d944726
MaybeDatapod <30482688+MaybeDatapod@users.noreply.github.com>
2019-06-04 03:18:02 -0500

    Add files via upload

--- NPCs/Boss/Databoss.cs
+++ NPCs/Boss/Databoss.cs
@@ -40,6 +40,16 @@ namespace DataMod.NPCs.Boss
         
         public override void BossLoot(ref string name, ref int potionType)
         {
+			if (!DataModWorld.downedDataboss)
+                {                                                          //Red  Green Blue
+                    Main.NewText("The ground sparkles with green", 100, 200, 100);  //this is the message that will appear when the npc is killed  , 200, 200, 55 is the text color
+                    for (int k = 0; k < (int)((double)(Main.maxTilesX * Main.maxTilesY) * 6E-05); k++)   //40E-05 is how many veins ore is going to spawn , change 40 to a lover value if you want less vains ore or higher value for more veins ore
+                    {
+                        int x = WorldGen.genRand.Next(0, Main.maxTilesX);
+                        int y = WorldGen.genRand.Next((int)WorldGen.rockLayer, Main.maxTilesY - 200); //this is the coordinates where the veins ore will spawn, so in Cavern layer
+                        WorldGen.TileRunner(x, y, (double)WorldGen.genRand.Next(3, 6), WorldGen.genRand.Next(2, 6), mod.TileType("DatapodOre"), false, 0f, 0f, false, true);
+                    }
+                }
 			DataModWorld.downedDataboss = true;
             potionType = ItemID.GreaterHealingPotion;   //boss drops
 			int choice = Main.rand.Next(4);
