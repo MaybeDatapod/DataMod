45a75dc6204c8aee0b2c0cadbbfe337633313b1b
MaybeDatapod <30482688+MaybeDatapod@users.noreply.github.com>
2019-06-04 02:15:25 -0500

    Add files via upload

---
+++ Items/DatapodBar.cs
@@ -0,0 +1,30 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DataMod.Items
+{
+    public class DatapodBar : ModItem
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Datapod Bar");
+		}
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 24;
+            item.maxStack = 999;
+            item.value = 8000;
+            item.rare = 7;
+        }
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.ItemType("DatapodOre"), 4);
+			recipe.AddTile(TileID.Hellforge);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+    }
+}
