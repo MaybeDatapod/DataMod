70ceaba1a948254f0450a69493971ae58bda5ba8
MaybeDatapod <30482688+MaybeDatapod@users.noreply.github.com>
2019-06-08 22:40:52 -0500

    Add files via upload

--- Projectiles/DatabombProj.cs
+++ Projectiles/DatabombProj.cs
@@ -41,7 +41,7 @@ namespace DataMod.Projectiles
 
                     if (Math.Sqrt(x * x + y * y) <= radius + 0.5)   //this make so the explosion radius is a circle
                     {
-                        WorldGen.KillTile(xPosition, yPosition, false, true, false);  //this make the explosion destroy tiles  
+                        WorldGen.KillTile(xPosition, yPosition, false, false, false);  //this make the explosion destroy tiles  
                         Dust.NewDust(position, 22, 22, DustID.Smoke, 0.0f, 0.0f, 120, new Color(), 1f);  //this is the dust that will spawn after the explosion
                     }
                 }
